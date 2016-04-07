4a42f237f7a3a0670e519d6d54d990fbd377b77a
PyryM <pkmatikainen@gmail.com>
2016-04-06 23:01:08 -0400

    initial version

---
+++ js/console-codemirror.js
@@ -0,0 +1,181 @@
+window.$ = window.jQuery = require('./lib/jquery-2.2.2.min.js');
+const ipcRenderer = require('electron').ipcRenderer;
+
+$(function(){
+    initCodeMirror();
+    setupHandlers();
+    printArt();
+});
+
+var repl = null;
+
+function remoteEval(code) {
+    ipcRenderer.send('eval', {"code": code});
+}
+
+function replLog(message, topic) {
+    $('<div class="log-' + topic + '">[' + topic + '] ' +
+        message +
+        '</div>').appendTo("#log-column");
+}
+
+function setupHandlers() {
+    ipcRenderer.on('print', function (event, arg) {
+        console.log(arg.message);
+        repl.print(arg.message);
+    });
+
+    ipcRenderer.on('log', function (event, arg) {
+        replLog(arg.message, arg.topic);
+    });
+
+    ipcRenderer.send('getserverinfo');
+
+    console.log("Handlers set up!");
+}
+
+function printArt() {
+    repl.print(" _                                      _ ");
+    repl.print("| |_ _ __ _   _ _ __ ___ _ __ ___ _ __ | |");
+    repl.print("| __| '__| | | | '__/ _ \\ '__/ _ \\ '_ \\| |");
+    repl.print("| |_| |  | |_| | | |  __/ | |  __/ |_) | |");
+    repl.print(" \\__|_|   \\__,_|_|  \\___|_|  \\___| .__/|_|");
+    repl.print("                                 |_|      ");
+}
+
+function bracketsBalanced(code) {
+    var length = code.length;
+    var delimiter = '';
+    var brackets = [];
+    var matching = {
+        ')': '(',
+        ']': '[',
+        '}': '{'
+    };
+
+    for (var i = 0; i < length; i++) {
+        var char = code.charAt(i);
+
+        switch (delimiter) {
+        case "'":
+        case '"':
+        default:
+            switch (char) {
+            case "'":
+            case '"':
+                delimiter = char;
+                break;
+            case "(":
+            case "[":
+            case "{":
+                brackets.push(char);
+                break;
+            case ")":
+            case "]":
+            case "}":
+                if (!brackets.length || matching[char] !== brackets.pop()) {
+                    repl.print(new SyntaxError("Unexpected closing bracket: '" + char + "'"), "error");
+                    return null;
+                }
+            }
+        }
+    }
+
+    return brackets.length ? false : true;
+}
+
+function doEndBalanced(code) {
+    var startTokens = new Set(["function", "do"]);
+    var endTokens = new Set(["end"]);
+
+    codeTokens = code.split(/\s+/);
+    var nestLevel = 0;
+    var curtoken;
+
+    for(var i = 0; i < codeTokens.length; ++i) {
+        curtoken = codeTokens[i];
+        if(startTokens.has(curtoken)) {
+            nestLevel += 1;
+        } else if(endTokens.has(curtoken)) {
+            nestLevel -= 1;
+        }
+
+        // allow !! to break out of a multiline entry
+        if(curtoken == "!!") {
+            return null;
+        }
+
+        // unrecoverable situation like "do [...] end end"
+        if(nestLevel < 0) {
+            return null;
+        }
+    }
+
+    return (nestLevel == 0);
+}
+
+function initCodeMirror() {
+    var geval = eval;
+
+    repl = new CodeMirrorREPL("repl", {
+        mode: "lua",
+        theme: "dracula"
+    });
+
+    $("#console-column").click(function() {
+        repl.mirror.focus();
+    });
+
+    window.print = function (message, mclass) {
+        repl.print(message, mclass || "message");
+    };
+
+    repl.isBalanced = function (code) {
+        var b0 = bracketsBalanced(code);
+        var b1 = doEndBalanced(code);
+        if(b0 == null || b1 == null) {
+            return null;
+        } else {
+            return b0 && b1;
+        }
+    };
+
+    repl.eval = function (code) {
+        remoteEval(code);
+    };
+
+    function isExpression(code) {
+        if (/^\s*function\s/.test(code)) return false;
+
+        try {
+            Function("return " + code);
+            return true;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    function express(value) {
+        if (value === null) var type = "Null";
+        else if (typeof value === "Undefined") var type = "Undefined";
+        else var type = Object.prototype.toString.call(value).slice(8, -1);
+
+        switch (type) {
+        case "String":
+            value = '"' + value.replace('\\', '\\\\').replace('\0', '\\0').replace('\n', '\\n').replace('\r', '\\r').replace('\t', '\\t').replace('\v', '\\v').replace('"', '\\"') + '"';
+        case "Number":
+        case "Boolean":
+        case "Function":
+        case "Undefined":
+        case "Null":
+            repl.print(value);
+            break;
+        case "Object":
+        case "Array":
+            repl.print(JSON.stringify(value, 4));
+            break;
+        default:
+            repl.print(value, "error");
+        }
+    }
+}
---
+++ main.js
@@ -0,0 +1,114 @@
+'use strict';
+
+const port = 8087;
+const WebSocketServer = require('ws').Server;
+
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
+// Module to communicate with render thread
+const ipcMain = electron.ipcMain;
+
+// Keep a global reference of the window object, if you don't, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow;
+
+// Keep a global reference to the websocket server and connection to truss
+let wss = null;
+let remoteConnection = null;
+
+function sendToRemote(channel, msg){
+  if(remoteConnection && remoteConnection.readyState == remoteConnection.OPEN){
+    msg["mtype"] = channel;
+    remoteConnection.send(JSON.stringify(msg));
+  } else {
+    sendToClient("print", {"message": "[no remote connection]"});
+  }
+}
+
+function sendToClient(channel, msg) {
+  mainWindow.webContents.send(channel, msg);
+}
+
+function startServer() {
+  // create the websocket server
+  wss = new WebSocketServer({ port: port });
+  console.log("serving on port " + port);
+
+  wss.on('connection', function connection(ws) {
+    var cname = ws._socket.remoteAddress + ":" +
+                ws._socket.remotePort;
+    sendToClient("print", {"message": "["  + cname + " connected.]"});
+    remoteConnection = ws;
+
+    ws.on('message', function incoming(message) {
+      var jdata = JSON.parse(message);
+      sendToClient(jdata.mtype, jdata);
+    });
+
+    ws.on('close', function(evt) {
+      sendToClient("print", {"message": "[" + cname + " disconnected.]"});
+      remoteConnection = null;
+    });
+  });
+}
+
+function createWindow () {
+  // Create the browser window.
+  mainWindow = new BrowserWindow({width: 800, height: 600});
+
+  // and load the index.html of the app.
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
+
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
+
+  // Emitted when the window is closed.
+  mainWindow.on('closed', function() {
+    // Dereference the window object, usually you would store windows
+    // in an array if your app supports multi windows, this is the time
+    // when you should delete the corresponding element.
+    mainWindow = null;
+  });
+
+  ipcMain.on('eval', function(event, arg) {
+    sendToRemote('eval', arg);
+  });
+
+  ipcMain.on('info', function(event, arg) {
+    sendToRemote('info', arg);
+  });
+
+  ipcMain.on('suggest', function(event, arg) {
+    sendToRemote('suggest', arg);
+  });
+
+  ipcMain.on('getserverinfo', function(event, arg) {
+    sendToClient('print', {'message': '[serving on port ' + port + ']'});
+  });
+
+  startServer();
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  // On OS X it's common to re-create a window in the app when the
+  // dock icon is clicked and there are no other windows open.
+  if (mainWindow === null) {
+    createWindow();
+  }
+});
